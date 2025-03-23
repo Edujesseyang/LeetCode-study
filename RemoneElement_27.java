50189489f7c81d12fb18a232d56e10031affd94b
Edujesseyang <edujesseyang@gmail.com>
2025-03-22 18:38:26 -0700

    update

---
+++ RemoneElement_27.java
@@ -0,0 +1,129 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoneElement_27 {
+    /*Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+    The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted,
+you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
+The remaining elements of nums are not important as well as the size of nums.
+Return k.
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int val = ...; // Value to remove
+int[] expectedNums = [...]; // The expected answer with correct length.
+                            // It is sorted with no values equaling val.
+
+int k = removeElement(nums, val); // Calls your implementation
+
+assert k == expectedNums.length;
+sort(nums, 0, k); // Sort the first k elements of nums
+for (int i = 0; i < actualLength; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+
+
+Example 1:
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+Note that the five elements can be returned in any order.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+
+Constraints:
+
+0 <= nums.length <= 100
+0 <= nums[i] <= 50
+0 <= val <= 100*/
+    public static void main(String[] args) {
+        List<int[]> test = new ArrayList<>();
+        test.add(new int[]{7, 1, 2, 2, 3, 3, 1, 5, 6, 8, 9, 6});
+        test.add(new int[]{0, 6, 5, 3, 7, 3, 4, 6, 5, 5, 9, 7});
+        test.add(new int[]{2, 7, 2, 2, 3, 3, 7, 5, 6, 7, 5, 8});
+        test.add(new int[]{5, 5, 6, 6, 8, 4, 5, 3, 5, 3, 9, 7});
+        test.add(new int[]{0, 1, 2, 2, 3, 3, 4, 5, 6, 8, 3, 7});
+
+
+        System.out.print("Test 1: ");
+        printTest(test.getFirst());
+        System.out.print("Output:   ");
+        printResult(test.getFirst(), removeElement(test.getFirst(), 2));
+        System.out.println("Expected: {7, 1, 3, 3, 1, 5, 6, 8, 9, 6, }");
+
+        System.out.print("\nTest 2: ");
+        printTest(test.get(1));
+        System.out.print("Output:   ");
+        printResult(test.get(1), removeElement(test.get(1), 5));
+        System.out.println("Expected: {0, 6, 3, 7, 3, 4, 6, 9, 7, }");
+
+        System.out.print("\nTest 3: ");
+        printTest(test.get(2));
+        System.out.print("Output:   ");
+        printResult(test.get(2), removeElement(test.get(2), 2));
+        System.out.println("Expected: {7, 3, 3, 7, 5, 6, 7, 5, 8, }");
+
+        System.out.print("\nTest 4: ");
+        printTest(test.get(3));
+        System.out.print("Output:   ");
+        printResult(test.get(3), removeElement(test.get(3), 5));
+        System.out.println("Expected: {6, 6, 8, 4, 3, 3, 9, 7, }");
+
+        System.out.print("\nTest 5: ");
+        printTest(test.get(4));
+        System.out.print("Output:   ");
+        printResult(test.get(4), removeElement(test.get(4), 7));
+        System.out.println("Expected: {0, 1, 2, 2, 3, 3, 4, 5, 6, 8, 3, }");
+
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+        return i;
+    }
+
+    public static void printTest(int[] nums) {
+        System.out.print("{");
+        for (int i : nums) {
+            System.out.print(i + ", ");
+        }
+        System.out.println("}");
+    }
+
+    public static void printResult(int[] nums, int k) {
+        System.out.print("{");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + ", ");
+        }
+        System.out.println("}");
+    }
+
+    // 解法:
+    /* 经典的双指针, 初始快慢针为零;
+        快针近loop, 如果快针指向值非为指定, swap.
+        然后跟新慢针, 再等下快针.
+     */
+}
---
+++ UnivaluedBinaryTree_965.java
@@ -0,0 +1,92 @@
+import javax.swing.tree.TreeNode;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class UnivaluedBinaryTree_965 {
+    /*A binary tree is uni-valued if every node in the tree has the same value.
+
+    Given the root of a binary tree, return true if the given tree is uni-valued, or false otherwise.
+    *
+            1
+            /\
+           1  1
+          /\   \
+         1  1   1    (True)
+
+            1
+            /\
+           1  1
+          /\  /\
+         5  1 1 1    (False)
+
+    Constraints:
+
+    The number of nodes in the tree is in the range [1, 100].
+    0 <= Node.val < 100
+
+    * */
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(1);
+        root1.left.left = new TreeNode(1);
+        root1.left.right = new TreeNode(1);
+        root1.right = new TreeNode(1);
+        root1.right.left = new TreeNode(1);
+        root1.right.right = new TreeNode(1);
+
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(1);
+        root2.left.left = new TreeNode(1);
+        root2.left.right = new TreeNode(1);
+        root2.right = new TreeNode(1);
+        root2.right.left = new TreeNode(5);
+        root2.right.right = new TreeNode(1);
+
+        System.out.println("Test case 1: \nExpected: True, output: " + univalueBTCheck(root1));
+        System.out.println("Test case 2: \nExpected: False, output: " + univalueBTCheck(root2));
+    }
+
+    public static boolean univalueBTCheck(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            if (current != null && current.val != root.val) {
+                return false;
+            } else {
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+        }
+        return true;
+    }
+
+    /*
+     * 任何简单的搜索都可以, 广度优先, 深度优先 都无所谓. */
+
+}
